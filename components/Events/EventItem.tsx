b15c7e8267a946daf032704de0651a87ccb61510
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-01 11:57:54 +0530

    Added the required css modules

--- components/Events/EventItem.tsx
+++ components/Events/EventItem.tsx
@@ -1,36 +1,32 @@
 import { Fragment } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { IDummyEvents } from "@/dummyData";
 
 interface Props {
-  event: {
-    title: string;
-    description: string;
-    location: string;
-    date: string;
-    image: string;
-  };
+  event: IDummyEvents;
 }
 
 const EventItem: React.FC<Props> = ({ event }) => {
-  const { title, description, location, date, image } = event;
+  const { id, title, description, location, date, image } = event;
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
     day: "numeric",
     month: "long",
     year: "numeric",
   });
   const formattedAddress = location.replace(",", "\n");
+  const exploreLink = `/events/${id}`;
   return (
     <Fragment>
       <h4>{title}</h4>
       <p>{description}</p>
       <address>{formattedAddress}</address>
       <p>{humanReadableDate}</p>
-      <Image
-        src={`http://localhost:3000/${image}`}
-        alt={title}
-        width={800}
-        height={500}
-      />
+      <Image src={"/" + image} alt={title} width={800} height={500} />
+      <Link href={exploreLink}>Explore</Link>
+      {/* For NextJS 13 <Link href={exploreLink}>
+        <a className="">Explore</a>
+      </Link> */}
     </Fragment>
   );
 };
