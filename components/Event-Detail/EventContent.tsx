f0b211644c5975840432c0e8dd7378404580cfd5
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-02 14:12:21 +0530

    Added Event Content Component

---
+++ components/Event-Detail/EventContent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from "./EventContent.module.css";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const EventContent: React.FC<Props> = ({ children }) => {
+  return <section className={classes.content}>{children}</section>;
+};
--- pages/events/[id].tsx
+++ pages/events/[id].tsx
@@ -1,4 +1,14 @@
+import { getEventById } from "@/dummyData";
+import { useRouter } from "next/router";
+
 const EventDetail = () => {
+  const router = useRouter();
+  const eventID = router.query.id;
+  if (!eventID) {
+    return <p>No event found</p>;
+  }
+  const event = getEventById(eventID.toString());
+  
   return (
     <div>
       <h1>Event Detail</h1>
