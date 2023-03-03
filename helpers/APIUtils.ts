0fa7d25f192b9b1eb173cc0392764d453d74135d
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-03 17:16:10 +0530

    Implemented getAllEvents function, to fetch the data from firebase

---
+++ helpers/APIUtils.ts
@@ -0,0 +1,32 @@
+interface Data {
+  title: string;
+  description: string;
+  image: string;
+  isFeatured: string;
+  location: string;
+  date: string;
+}
+
+export const getFeaturedEvents = async () => {
+  const allEvents = await getAllEvents();
+  return events.filter((event) => event.isFeatured);
+};
+
+export const getAllEvents = async () => {
+  const response = await fetch(
+    "https://star-wars-f4c01-default-rtdb.firebaseio.com/Events.json"
+  );
+  const data: Data[] = await response.json();
+  let events = Object.entries(data).map(
+    ([id, { title, description, image, isFeatured, location, date }]) => ({
+      id,
+      title,
+      description,
+      image,
+      isFeatured,
+      location,
+      date,
+    })
+  );
+  return events;
+};
