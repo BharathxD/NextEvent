7fc3940ec947575b80654c53f05f088d006f54eb
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-03 18:19:25 +0530

    Implemented Server Side Rendering for filtering events [using slug]

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,30 +1,26 @@
 import { useRouter } from "next/router";
 import EventList from "@/components/Events/EventList";
-import { getAllEvents } from "@/helpers/APIUtils";
+import {
+  IEvents,
+  getAllEvents,
+  getEventById,
+  getFilteredEvents,
+} from "@/helpers/APIUtils";
+import { FC } from "react";
 
-const FilteredEvents = () => {
+interface Props {
+  hasError: boolean;
+  events: IEvents[];
+}
+
+const FilteredEvents: FC<Props> = ({ hasError, events }) => {
   const router = useRouter();
-  const filteredData = router.query.slug;
-  if (!filteredData) {
-    return <p>Found no events</p>;
-  }
-  const { year, month } = {
-    year: +filteredData[0],
-    month: +filteredData[1],
-  };
-  if (isNaN(year) || isNaN(month) || year > 2023 || year < 2021) {
-    return <p>Invalid filter, enter the correct values</p>;
-  }
-  const filteredEvents = getFilteredEvents({
-    year: year,
-    month: month,
-  });
-  if (!filteredEvents || filteredEvents.length === 0) {
+  if (!events || events.length === 0) {
     return <p>No events found</p>;
   }
   return (
     <div>
-      <EventList featured={filteredEvents} />
+      <EventList featured={events} />
     </div>
   );
 };
@@ -39,13 +35,23 @@ export const getStaticProps = async (context: { params: { id: string } }) => {
   };
 };
 
-export const getStaticPaths = async () => {
-  const events = await getAllEvents();
-  const paths = events.map((event) => ({ params: { id: event.id } }));
-  debugger;
+export const getServerSideProps = async (context: { params: any }) => {
+  const { params } = context;
+  const events = params.slug;
+  const { year, month } = {
+    year: +events[0],
+    month: +events[1],
+  };
+  if (isNaN(year) || isNaN(month) || year > 2023 || year < 2021) {
+    return { props: { hasError: true } };
+    // return { notFound: true };
+  }
+  const filteredEvents = await getFilteredEvents({
+    year: year,
+    month: month,
+  });
   return {
-    paths: paths,
-    fallback: true,
+    props: { evetns: filteredEvents },
   };
 };
 
--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -33,6 +33,7 @@ export const getStaticProps = async () => {
     props: {
       events: event,
     },
+    revalidate: 60,
   };
 };
 
